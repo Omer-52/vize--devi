72980caf9ea49ac417bfef5c7644c3d8a0aa338c
Ömer Özsu <ozsuomer67@gmail.com>
2026-04-08 23:58:48 +0300

    genel ayarlar eklendi (final)

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.background-slideshow img');
     let currentImageIndex = 0;
 
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
     function changeImage() {
         images[currentImageIndex].classList.remove('active');
         currentImageIndex = (currentImageIndex + 1) % images.length;
@@ -25,5 +28,33 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     animateSkillBars(); 
-    setInterval(changeImage, 5000); 
+    setInterval(changeImage, 5000);
+
+    // Function to set the theme and update button icon
+    function setTheme(theme) {
+        body.dataset.theme = theme;
+        localStorage.setItem('theme', theme);
+       
+        if (theme === 'dark') {
+            themeToggleButton.innerHTML = '☀️'; 
+            themeToggleButton.setAttribute('aria-label', 'Switch to light theme');
+        } else {
+            themeToggleButton.innerHTML = '🌙'; 
+            themeToggleButton.setAttribute('aria-label', 'Switch to dark theme');
+        }
+    }
+
+  
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        setTheme(savedTheme);
+    } else {
+        setTheme('dark'); 
+    }
+
+    themeToggleButton.addEventListener('click', function() {
+        const currentTheme = body.dataset.theme;
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+    });
 });
